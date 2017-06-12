5ee23c8bed8d9e53526bf015d76b434ef397175c
kuniyoshitatsuya <kuniyoshi.tatsuya@m.alhinc.jp>
2017-06-12 14:44:03 +0900

    repush kuniyoshi

--- src/admin/controller/EditUserServlet.java
+++ src/admin/controller/EditUserServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/admin/userEdit"})
+@WebServlet(urlPatterns = {"/admin/editUser"})
 public class EditUserServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -17,6 +17,6 @@ public class EditUserServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
 
-		request.getRequestDispatcher("/admin/userEdit.jsp").forward(request, response);
+		request.getRequestDispatcher("/admin/editUser.jsp").forward(request, response);
 	}
 }
