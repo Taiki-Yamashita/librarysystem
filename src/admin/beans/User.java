6b3e92dc4762ec7b256239d80253474409656285
yamashita.taiki <yamashita.taiki@m.alhinc.jp>
2017-06-13 11:45:45 +0900

    taiki4

---
+++ src/admin/beans/User.java
@@ -0,0 +1,68 @@
+package admin.beans;
+
+public class User {
+	private int id;
+	private String name;
+	private String address;
+	private String tel;
+	private String mail;
+	private String point;
+	private String registerDate;
+	private String libraryId;
+	private boolean stopping;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPoint() {
+		return point;
+	}
+	public void setPoint(String point) {
+		this.point = point;
+	}
+	public String getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+	public String getLibraryId() {
+		return libraryId;
+	}
+	public void setLibraryId(String libraryId) {
+		this.libraryId = libraryId;
+	}
+	public boolean isStopping() {
+		return stopping;
+	}
+	public void setStopping(boolean stopping) {
+		this.stopping = stopping;
+	}
+}
---
+++ src/utils/Streamutil.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import exception.IORuntimeException;
+
+/**
+ * ストリーム関係のユーティリティー
+ */
+public class Streamutil {
+	/**
+	 * input から outputにデータをコピーします。
+	 *
+	 * @param input
+	 * @param output
+	 */
+
+	public static void copy(InputStream input, OutputStream output) {
+		System.out.println(input);
+		System.out.println(output);
+		byte[] buffer = new byte[4096];
+		try {
+			for (int n = 0; -1 != (n = input.read(buffer));) {
+				output.write(buffer, 0, n);
+			}
+		} catch (IOException e) {
+			throw new IORuntimeException(e);
+		}
+	}
+}
