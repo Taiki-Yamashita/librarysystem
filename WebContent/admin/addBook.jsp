a19b3211dc2a569d91145b98e057ac5df4244b3d
okadakosuke <okada.kosuke@m.alhinc.jp>
2017-06-13 14:41:21 +0900

    addBook okada6/13/14/41

--- WebContent/admin/addBook.jsp
+++ WebContent/admin/addBook.jsp
@@ -8,5 +8,40 @@
 </head>
 <body>
 	<a href = "../">トップ</a>
+
+
+<form action = "./addBook"method = "post"><br />
+	<label for = "name">名前</label><br>
+	<input name = "name" id = "name" value = "name"/><br />
+
+	<label for = "author">著者</label><br>
+	<input name = "author" id = "author" value = "author"/><br />
+
+	<label for = "publisher">出版社</label><br>
+	<input name = "publisher" id = "publisher"/><br />
+
+	<label for = "category">カテゴリ</label><br>
+	<input name = "category" id = "category" value = "category"/><br />
+
+	<label for = "type">タイプ</label><br>
+	<input name = "type" id = "type" value = "type"/><br />
+
+	<label for = "publishedDate">出版日</label><br>
+	<input name = "publishedDate" id = "publishedDate"/><br />
+
+	<label for = "libraryId">図書館</label><br>
+	<input name = "libraryId" id = "libraryId" value = "libraryId"/><br />
+
+	<label for="shelfId">棚番号</label><br>
+	<input name = "shelfId" id = "shelfId" value = "shelfId"/><br />
+
+	<label for = "isbnId">ISBN番号</label><br>
+	<input name = "isbnId" id = "isbnId"/><br />
+
+
+
+	<input type="submit" value="登録" />
+
+	</form>
 </body>
 </html>
--- src/admin/controller/AddBookServlet.java
+++ src/admin/controller/AddBookServlet.java
@@ -8,9 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Book;
+import service.BookService;
+
+
 @WebServlet(urlPatterns = {"/admin/addBook"})
 public class AddBookServlet extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
@@ -18,5 +21,26 @@ public class AddBookServlet extends HttpServlet {
 			HttpServletResponse response) throws IOException, ServletException {
 
 		request.getRequestDispatcher("/admin/addBook.jsp").forward(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,
+	ServletException{
+
+		Book book = new Book();
+		book.setName(request.getParameter("name"));
+		book.setAuthor(request.getParameter("author"));
+		book.setPublisher(request.getParameter("publisher"));
+		book.setCategory(request.getParameter("category"));
+		book.setType(request.getParameter("type"));
+		book.setPublishedDate(request.getParameter("publishedDate"));
+		book.setLibraryId(request.getParameter("libraryId"));
+		book.setShelfId(request.getParameter("shelfId"));
+		book.setIsbnId(request.getParameter("isbnId"));
+
+		new BookService().insert(book);
+
+
+
+		response.sendRedirect("./admin/addBook");
 	}
 }
