038f97668743b8853b232791525602df721dcf1e
kuniyoshitatsuya <kuniyoshi.tatsuya@m.alhinc.jp>
2017-06-20 15:23:30 +0900

    afsd

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -15,20 +15,7 @@ public class DBUtil {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/library_system";
 	private static final String USER = "root";
-	//private static final String PASSWORD = "Okada0121washo";
-<<<<<<< HEAD
-=======
-	private static final String PASSWORD = "H06-m10-d31";
-	//private static final String PASSWORD = "Okada0121washo";
->>>>>>> 521198ce61c26841df9728a7d1a3a31c54b3f240
-	//private static final String PASSWORD = "H06-m10-d31";
-
-	//private static final String PASSWORD = "9244Tatsuya";
-
-	//private static final String PASSWORD = "Okada0121washo";
-
-
-
+	private static final String PASSWORD = "9244Tatsuya";
 
 	static {
 
