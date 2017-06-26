61768543dbd74020bda4b01932ea2d37f7e0a518
okadakosuke <okada.kosuke@m.alhinc.jp>
2017-06-26 13:51:10 +0900

    agdf

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -15,11 +15,11 @@ public class DBUtil {
 	private static final String URL = "jdbc:mysql://localhost:3306/library_system";
 	private static final String USER = "root";
 	//private static final String PASSWORD = "9244Tatsuya";
-<<<<<<< HEAD
-	private static final String PASSWORD = "H06-m10-d31";
-=======
+
+	//private static final String PASSWORD = "H06-m10-d31";
+
 	private static final String PASSWORD = "Okada0121washo";
->>>>>>> 796f9dcc7d6ba561149e1886baf292a1e068e989
+
 
 
 	static {
