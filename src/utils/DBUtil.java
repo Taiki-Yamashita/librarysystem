46b1c638125dd92ec3c7791dd51c2fb73e0d2363
yamashita.taiki <yamashita.taiki@m.alhinc.jp>
2017-06-26 17:15:02 +0900

    dfzadfa

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -14,18 +14,11 @@ public class DBUtil {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/library_system";
 	private static final String USER = "root";
-<<<<<<< HEAD
-	private static final String PASSWORD = "9244Tatsuya";
-=======
+	//private static final String PASSWORD = "9244Tatsuya";
 	//private static final String PASSWORD = "9244Tatsuya";
 	//private static final String PASSWORD = "Okada0121washo";
 
 	private static final String PASSWORD = "H06-m10-d31";
-<<<<<<< HEAD
-=======
->>>>>>> 74cc4540eec5c613129ef4b49dec5e59e6a1b3b8
->>>>>>> fbd29c7a82db2ec6952181223a63b8e6f5fedce1
->>>>>>> 8d774768a022a9b884960e743f3b2fef65424927
 	static {
 
 		try {
